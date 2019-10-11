76df8e6c3e0834f4e9952c08a56b5063005c4437
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-10-11 23:02:06 +0000

    Update docs after building Travis build 9 of vstadnytskyi/syringe-pump
    The docs were built from the branch 'master' against the commit
    fc3b7fb25358fec1d4a9c9a77514d9e3b340a568.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/vstadnytskyi/syringe-pump/jobs/596820405.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

---
+++ _sources/driver.rst.txt
@@ -0,0 +1,12 @@
+======
+Driver
+======
+
+Start by importing Cavro Centris Syringe Pump.
+
+.. code-block:: python
+
+  from syringe_pump.driver import Driver
+
+.. autoclass:: syringe_pump.driver.Driver
+  :members:
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1.post4+g7ecae9e',
+    VERSION: '0.0.1.post5+gfc3b7fb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
