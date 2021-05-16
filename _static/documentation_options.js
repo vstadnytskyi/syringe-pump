fdcb25263e2a4c0b8123fb44fe18f374283e074f
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-05-16 13:12:12 +0000

    Update docs after building Travis build 56 of vstadnytskyi/syringe-pump
    The docs were built from the branch 'master' against the commit
    590589ed571d43158759ebf2b0b4bc8804f624fb.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/vstadnytskyi/syringe-pump/jobs/771316175.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.13.post9+g5e53597',
+    VERSION: '0.0.13.post10+g590589e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
